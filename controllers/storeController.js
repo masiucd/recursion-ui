596d0f22940b2fc24b1e5eefb828a52800695d94
Masiu <ciszekmarcell@gmail.com>
2019-08-10 22:51:50 +0200

    add store form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,14 @@
+const Store = require('../models/Store');
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 };
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {
+    title: 'Add Store',
+  });
+};
+
+exports.createStore = (req, res) => {};
