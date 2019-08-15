bcf10fa071b59f99f8769df8671d5424baae8d88
Masiu <ciszekmarcell@gmail.com>
2019-08-15 20:21:12 +0200

    edit store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose');
 
 const Store = mongoose.model('Store');
 
-exports.homePage = (req, res) => {
-  console.log(req.name);
-
-  res.render('index');
-};
-
 exports.addStore = (req, res) => {
   res.render('editStore', {
     title: 'Add Store',
@@ -29,6 +23,23 @@ exports.createStore = async (req, res) => {
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
-  console.log(stores);
   res.render('stores', { title: 'stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  // res.json(store);
+  res.render('editStore', { title: `edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  }).exec();
+  req.flash(
+    'success',
+    `Successfully updated  <strong>${store.name}</strong> <a href="/stores/${store.slug}"> View Store </a> `
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
