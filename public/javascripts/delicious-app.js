bac826bebd473c6393ccd0ce0b246a194f2f7924
Masiu <ciszekmarcell@gmail.com>
2019-08-31 14:43:27 +0200

    ajax part1

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,5 +1,7 @@
 import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import autoComplete from './modules/autocomplete';
+import typeAhead from './modules/typeAhead';
 
 autoComplete($('#address'), $('#lat'), $('#lng'));
+typeAhead($('.search'));
