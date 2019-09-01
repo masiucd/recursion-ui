59d190588b4b82ed1d64a7633e8a4e767889cc85
Masiu <ciszekmarcell@gmail.com>
2019-09-01 13:41:11 +0200

    geolocation

--- models/Store.js
+++ models/Store.js
@@ -33,7 +33,7 @@ const storeSchema = Schema({
     coordinates: [
       {
         type: Number,
-        required: 'You must supply coordinates ',
+        required: 'You must supply coordinates!',
       },
     ],
     address: {
@@ -54,6 +54,7 @@ storeSchema.index({
   name: 'text',
   description: 'text',
 });
+// storeSchema.index({ location: '2dsphere' });
 
 storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
