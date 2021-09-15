9975a3358754897dc24618f9ffb79945d62252f0
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-09-15 08:35:48 +0200

    test

--- src/util/getData.ts
+++
@@ -1,8 +0,0 @@
-import data from "../data/tree-data.json"
-
-export const getTreeData = () => {
-  return data.map((x) => ({
-    ...x,
-    hasChildren: data.filter((item) => item.parentId === x.id).length > 0,
-  }))
-}
